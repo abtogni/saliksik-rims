b02fbe6d25b4e9a19df4ac4b6f6a2faa9c54c825
kiiyokami <hiimkaze@gmail.com>
2023-09-11 12:38:38 +0800

    separation of files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
-import Home from './routes/home';
-import Login from './routes/auth/login';
-import Register from './routes/auth/register';
+import router from './router';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
