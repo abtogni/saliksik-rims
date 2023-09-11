82e4027b5daba3011cc49085312493ab576f4579
kiiyokami <hiimkaze@gmail.com>
2023-09-11 12:04:35 +0800

    boilerplate created

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import { ThemeProvider } from "@material-tailwind/react";
+import Sample from './routes/sample';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Sample />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+    <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ client/src/routes/sample.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from "@material-tailwind/react";
+
+function sample() {
+    return <Button>Button</Button>;
+}
+
+export default sample
