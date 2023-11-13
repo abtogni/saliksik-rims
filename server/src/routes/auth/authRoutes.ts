b48486c70567d425c0cb24feee312a8da893f4bb
Andrei Togni <105876892+abtogni@users.noreply.github.com>
2023-11-13 12:15:14 +0800

    finish backend routes

--- server/src/routes/auth/authRoutes.ts
+++ server/src/routes/auth/authRoutes.ts
@@ -1,11 +1,12 @@
-import { Router }  from 'express';
-import { getUsers, createUser, getUser } from '../../controllers/auth/userController';
+import { Request, Response, Router } from 'express'
+import { getUsers, createUser, getUser, updateUser } from '../../controllers/auth/userController';
 
 const router = Router();
 
 router.get('/getUsers', getUsers);
 router.get('/getUser', getUser);
 router.post('/createAccount', createUser);
+router.post('/updateAccount', updateUser);
 
 
 export default router;
