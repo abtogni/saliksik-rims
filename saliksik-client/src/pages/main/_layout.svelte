934e28433eded25ecf32fdd2dac4d8b882970374
Jomari Evangelista Valenciano <jomari.valenciano@unc.edu.ph>
2023-12-09 12:24:30 +0800

    Input fields and other changes

--- saliksik-client/src/pages/main/_layout.svelte
+++ saliksik-client/src/pages/main/_layout.svelte
@@ -1,80 +1,75 @@
 <script lang="ts">
-  import UNCLogo from '/login.png';
-  import { Sidebar, SidebarBrand, SidebarItem, SidebarWrapper, SidebarGroup, SidebarDropdownWrapper, SidebarDropdownItem } from 'flowbite-svelte';
-  import Menu from '../../assets/menu.svelte';
-  import { goto } from '@roxi/routify';
-  import { onMount } from 'svelte';
-  import { userData, isAuthenticated, updateUser } from '../../components/store';
-  import { BookOutline, CogOutline, UserOutline } from 'flowbite-svelte-icons';
+  import UNCLogo from "/login.png";
+  import { Sidebar, SidebarBrand, SidebarItem, SidebarWrapper, SidebarGroup, SidebarDropdownWrapper, SidebarDropdownItem } from "flowbite-svelte";
+  import Menu from "../../assets/menu.svelte";
+  import { goto } from "@roxi/routify";
+  import { onMount } from "svelte";
+  import { userData, isAuthenticated, updateUser } from "../../components/store";
+  import { BookOutline, CogOutline, UserOutline } from "flowbite-svelte-icons";
 
   var researches: any;
   let loading = true;
-  let error: string | null = null; 
+  let error: string | null = null;
 
   let currentUser: any;
 
   async function fetchUser() {
-  try {
-    const response = await fetch('/api/checkUser');
-    const user = await response.json();
-    updateUser({
-      _id: user.user._id,
-      userType: user.user.userType,
-      firstName: user.user.firstName,
-      lastName: user.user.lastName,
-      avatar: user.user.firstName.charAt(0) + user.user.lastName.charAt(0),
-      researches: user.user.researches
-    });
-  } catch (e) {
-    $goto('/');
+    try {
+      const response = await fetch("/api/checkUser");
+      const user = await response.json();
+      updateUser({
+        _id: user.user._id,
+        userType: user.user.userType,
+        firstName: user.user.firstName,
+        lastName: user.user.lastName,
+        avatar: user.user.firstName.charAt(0) + user.user.lastName.charAt(0),
+        researches: user.user.researches,
+      });
+    } catch (e) {
+      $goto("/");
+    }
   }
-}
 
   async function fetchResearches(userID: string) {
-        try {
-            const response = await fetch(`/api/research/getUserResearches?userID=${userID}`);
+    try {
+      const response = await fetch(`/api/research/getUserResearches?userID=${userID}`);
 
-            if (response.ok) {
-                researches = await response.json();
-            } else {
-                console.error('Failed to fetch researches');
-                error = 'Failed to fetch researches';
-            }
-        } catch (err) {
-            console.error('Error fetching researches:', err);
-            error = 'Error fetching researches';
-        } finally {
-            loading = false;
-        }
+      if (response.ok) {
+        researches = await response.json();
+      } else {
+        console.error("Failed to fetch researches");
+        error = "Failed to fetch researches";
+      }
+    } catch (err) {
+      console.error("Error fetching researches:", err);
+      error = "Error fetching researches";
+    } finally {
+      loading = false;
     }
+  }
 
-
-    onMount(async () => {
-        if (!$isAuthenticated) {
-          $goto('/login');
-        } else if ($isAuthenticated) {
-          await fetchUser().then (async () => {
-            currentUser = $userData;
-          });
-          if (currentUser) {
-            fetchResearches(currentUser._id);
-          } else {
-            console.error('Network error:', error);
-          }
-        }
-    });
-
-
+  onMount(async () => {
+    if (!$isAuthenticated) {
+      $goto("/login");
+    } else if ($isAuthenticated) {
+      await fetchUser().then(async () => {
+        currentUser = $userData;
+      });
+      if (currentUser) {
+        fetchResearches(currentUser._id);
+      } else {
+        console.error("Network error:", error);
+      }
+    }
+  });
 
   let site = {
-    name: 'Saliksik',
-    href: '/',
+    name: "Saliksik",
+    href: "/",
     img: UNCLogo,
   };
-  let spanClass = 'flex-1 ml-3 whitespace-nowrap';
+  let spanClass = "flex-1 ml-3 whitespace-nowrap";
 </script>
-
-
 
 <div class="sidebar h-full">
   <button data-drawer-target="separator-sidebar" data-drawer-toggle="separator-sidebar" aria-controls="separator-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover-bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover-bg-gray-700 dark:focus:ring-gray-600">
@@ -86,9 +81,8 @@
     <SidebarWrapper>
       <SidebarBrand {site} />
       <SidebarGroup>
-          {#if currentUser}
-          <SidebarItem label={`Hello, ${currentUser.firstName}`} {spanClass}>
-          </SidebarItem>
+        {#if currentUser}
+          <SidebarItem label={`Hello, ${currentUser.firstName}`} {spanClass}></SidebarItem>
         {:else}
           <div></div>
         {/if}
@@ -96,27 +90,27 @@
           <svelte:fragment slot="icon">
             <CogOutline />
           </svelte:fragment>
-          <SidebarDropdownItem label="Check Presentations" href='/main/admin/checkPresentations' />
-          <SidebarDropdownItem label="Check Proposals" href='/main/admin/checkProposals'/>
-          <SidebarDropdownItem label="Create User Accounts" href='/main/admin/createAccount' />
+          <SidebarDropdownItem label="Check Presentations" href="/main/admin/checkPresentations" />
+          <SidebarDropdownItem label="Check Proposals" href="/main/admin/checkProposals" />
+          <SidebarDropdownItem label="Create User Accounts" href="/main/admin/createAccount" />
+          <SidebarDropdownItem label="Dashboard" href="/main/dashboard" />
         </SidebarDropdownWrapper>
         {#if loading}
-        <SidebarItem label="Loading..."></SidebarItem>
+          <SidebarItem label="Loading..."></SidebarItem>
         {:else if researches}
-        <SidebarDropdownWrapper label="Researches">
-          <svelte:fragment slot="icon">
-            <BookOutline />
-          </svelte:fragment>
+          <SidebarDropdownWrapper label="Dashboard">
+            <svelte:fragment slot="icon">
+              <BookOutline />
+            </svelte:fragment>
+            
             <SidebarDropdownItem href="/main/createResearch" label="Create Research"></SidebarDropdownItem>
-        {#each researches as r}
-            <SidebarDropdownItem class="truncate" label={r.researchTitle} href={`/main/${r._id}`}></SidebarDropdownItem>
-        {/each}
-        </SidebarDropdownWrapper>
+            {#each researches as r}
+              <SidebarDropdownItem class="truncate" label={r.researchTitle} href={`/main/${r._id}`}></SidebarDropdownItem>
+            {/each}
+          </SidebarDropdownWrapper>
         {:else}
-        
-        <SidebarItem href="/main/createResearch" label="Create Research"></SidebarItem>
+          <SidebarItem href="/main/createResearch" label="Create Research"></SidebarItem>
         {/if}
-
       </SidebarGroup>
       <SidebarGroup border>
         {#if currentUser}
@@ -125,9 +119,7 @@
               <UserOutline />
             </svelte:fragment>
           </SidebarItem>
-          <SidebarItem href="/logout" label="Logout" {spanClass}>
-            
-          </SidebarItem>
+          <SidebarItem href="/logout" label="Logout" {spanClass}></SidebarItem>
         {:else}
           <div></div>
         {/if}
@@ -136,10 +128,10 @@
   </Sidebar>
 </div>
 
-<div class="pl-72 pr-10 pt-12">
+<div class="pl-72 pr-10 pt-6">
   {#if currentUser}
-    <slot scoped={{ userID: currentUser._id }}></slot>
+    <slot scoped={{ userID: currentUser._id }} />
   {:else}
-    <slot></slot>
+    <slot />
   {/if}
 </div>
---
+++ saliksik-client/src/pages/main/dashboard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Heading } from "flowbite-svelte";
+
+  
+</script>
+
+<main>
+  <Heading tag="h1" class="mb-6 text-xl">Dashboard</Heading>
+
+</main>
