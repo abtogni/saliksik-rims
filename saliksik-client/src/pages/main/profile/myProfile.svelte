d314826710ae3cc2e783a31df2e073e91144c4df
John Ric Z. Borja <102026838+johnric22@users.noreply.github.com>
2023-12-23 13:58:03 +0800

    still working on profile

--- saliksik-client/src/pages/main/profile/myProfile.svelte
+++ saliksik-client/src/pages/main/profile/myProfile.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import { userData } from '../../../components/store';
     import { Card, Avatar, Button } from 'flowbite-svelte';
-    
+   
+  import { Navbar, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
   </script>
-  <main>
+  <main class="h-screen bg-white">
    <div class="translate-x-1/4 mt-10 container">
   
     {#if $userData}
-    <Card padding="sm">
+    <Card padding="sm" class="left: 200">
       <div class="flex flex-col items-center pb-4">
         <Avatar size='xl'><h1 class="text-5xl"> {$userData.firstName.charAt(0) + $userData.lastName.charAt(0)} </h1></Avatar>
         <h1 class="mb-1 text-3xl font-medium text-gray-900 dark:text-white">{`${$userData.firstName} ${$userData.lastName}`}</h1>
@@ -17,7 +19,16 @@
         </div>
       </div>
     </Card>
-    {/if}
+    {/if} 
    </div>
+   <Navbar class="">
+    <NavHamburger />
+    <NavUl class="flex">
+      <NavLi class="flex-1 mr-2 text-center border border-blue-500 hover:bg-button rounded py-2 px-4" href="/">Personal Information</NavLi>
+      <NavLi href="/docs/components/navbar">Research Outputs</NavLi>
+      <NavLi href="/docs/components/accordion">Task and Notification Management</NavLi>
+      <NavLi href="/docs/components/alert">Funding Information</NavLi>
+    </NavUl>
+  </Navbar>
   </main>
   
