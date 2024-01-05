4fd2c23957eeb2957966d06a7858e2b8be7141a7
John Ric Z. Borja <102026838+johnric22@users.noreply.github.com>
2024-01-05 18:48:24 +0800

    some changes in profile

--- saliksik-client/src/pages/main/profile/Nav.svelte
+++ saliksik-client/src/pages/main/profile/Nav.svelte
@@ -1,14 +1,14 @@
 <script context="module">
-    // main navigation for the application
-    import Home from './Task.svelte';
-    import Page2 from './FundingInformation.svelte';
-    import About from './ResearchOutputs.svelte';
+    // main navigation
+    import Home from './Research.svelte';
+    import Page2 from './Stats.svelte';
+    import About from './Profile.svelte';
     
     // these are our 'pages' to navigate through (in order for this export to work the script tag MUST include context="module")
     export const navOptions = [
-            { page: 'Reasearch Outputs',   component: About },
-        { page: 'Task',   component: Home },
-        { page: 'Funding Information',   component: Page2 },
-            // other navigation pages can go here
+            { page: 'Profile',   component: About },
+        { page: 'Research',   component: Home },
+        { page: 'Stats',   component: Page2 },
+            
     ];
     </script>
---
+++ saliksik-client/src/pages/main/profile/Profile.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Textarea, Label, Input } from 'flowbite-svelte';
+</script>
+
+
+<Label for="textarea-id" class="mb-2 text-2xl">Introduction</Label>
+<Textarea id="textarea-id" placeholder="Introduce yourself and your Research" rows="4" name="message" />
+
+<Label class="block space-y-2">
+    <span class="text-2xl">Your email</span>
+    <Input label="Email" id="email" name="email" required placeholder="name@example.com" />
+  </Label>
+
+  <div>
+    <Label for="first_name" class="mb-2 text-2xl">Skills and expertise</Label>
+    <Input type="text" id="first_name" placeholder="Enter Your Skills and expertise" required />
+  </div>
+  <div>
+    <Label for="last_name" class="mb-2 text-2xl">Languages</Label>
+    <Input type="text" id="last_name" placeholder=" Enter your Languages" required />
+  </div>
