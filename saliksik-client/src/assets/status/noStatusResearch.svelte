96ed2f2529d04d8239bab09bd9124b8ac422614a
Jomari Evangelista Valenciano <jomari.valenciano@unc.edu.ph>
2024-01-27 15:15:28 +0800

    statuses

--- saliksik-client/src/assets/status/noStatusResearch.svelte
+++ saliksik-client/src/assets/status/noStatusResearch.svelte
@@ -8,17 +8,17 @@ import { CheckCircleOutline, InfoCircleOutline } from "flowbite-svelte-icons";
     <Tooltip arrow={false} class="rounded-md">Create and submit a concept note to start. Or submit an already existing/published research paper.</Tooltip>
 </Badge>
 <Badge border large class="flex items-center gap-2">
-    <Indicator color="orange" size="md" class="" />Waiting For Approval
+    <Indicator color="orange" size="md" class="" />Concept Note (For Approval)
     <InfoCircleOutline color="dark" size="sm" />
     <Tooltip arrow={false} class="rounded-md">Your concept note is in approval. A notice to proceed will be sent to your notification, once approved.</Tooltip>
 </Badge>
 <Badge border large color="blue" class="flex items-center gap-2">
-    <Indicator color="blue" size="md" class="" />Approved
+    <Indicator color="blue" size="md" class="" />Concept Note (Approved)
     <CheckCircleOutline color="dark" size="sm" />
     <Tooltip arrow={false} class="rounded-md">Your concept note is approved. A notice to present will be sent to your notification, once the presentation schedule is set.</Tooltip>
 </Badge>
 <Badge border large color="red" class="flex items-center gap-2">
-    <Indicator color="red" size="md" class="" />Rejected
+    <Indicator color="red" size="md" class="" />Concept Note (Rejected)
     <InfoCircleOutline color="dark" size="sm" />
     <Tooltip arrow={false} class="rounded-md">Your concept note is rejected. You can make revisions and resubmit the concept note again.</Tooltip>
 </Badge>
@@ -34,7 +34,7 @@ import { CheckCircleOutline, InfoCircleOutline } from "flowbite-svelte-icons";
 </Badge>
 <Badge border large color="red" class="flex items-center gap-2">
     <Indicator color="red" size="md" class="" />Initial Presentation (Not Going)
-    <CheckCircleOutline color="dark" size="sm" />
+    <InfoCircleOutline color="dark" size="sm" />
     <Tooltip arrow={false} class="rounded-md">A new schedule will be sent to your notification, once a new presentation schedule is set.</Tooltip>
 </Badge>
 <Badge border large color="red" class="flex items-center gap-2">
@@ -49,6 +49,11 @@ import { CheckCircleOutline, InfoCircleOutline } from "flowbite-svelte-icons";
 </Badge>
 <Badge border large color="red" class="flex items-center gap-2">
     <Indicator color="red" size="md" class="" />Incentive (Released)
+    <CheckCircleOutline color="dark" size="sm" />
+    <Tooltip arrow={false} class="rounded-md">You can now claim your incentive in the UNC Accounting Office.</Tooltip>
+</Badge>
+<Badge border large color="blue" class="flex items-center gap-2">
+    <Indicator color="blue" size="md" class="" />Published
     <CheckCircleOutline color="dark" size="sm" />
     <Tooltip arrow={false} class="rounded-md">You can now claim your incentive in the UNC Accounting Office.</Tooltip>
 </Badge>
