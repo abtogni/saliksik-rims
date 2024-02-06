73866f16b6d44eeec9be34c043cddaf8d7e651a2
Jomari Evangelista Valenciano <jomari.valenciano@unc.edu.ph>
2024-02-06 12:52:58 +0800

    pahabol

--- client/components.d.ts
+++ client/components.d.ts
@@ -9,7 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     Concept_note_overview: typeof import('./src/components/pages/admin/concept_note_overview.vue')['default']
     Concept_note_submissions: typeof import('./src/components/pages/admin/concept_note_submissions.vue')['default']
-    Create_user: typeof import('./src/components/pages/admin/create_user.vue')['default']
     Create_user_modal: typeof import('./src/components/pages/admin/create_user_modal.vue')['default']
     Login: typeof import('./src/components/pages/Login.vue')['default']
     Nav_bar: typeof import('./src/components/nav_bar.vue')['default']
