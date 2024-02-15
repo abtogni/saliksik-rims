507418baee62941d04c96cb3ef6c44de0aeaca22
Kiiyo <85468568+kiiyokami@users.noreply.github.com>
2024-02-15 17:17:36 +0800

    navbar

--- client/src/components/pages/nav_bar.vue
+++ client/src/components/pages/nav_bar.vue
@@ -8,10 +8,10 @@
     <v-divider></v-divider>
 
     <v-list density="compact" nav>
-      <v-list-item prepend-icon="mdi-folder-multiple-outline" title="All Research Projects" value="myfiles"
-        href=""></v-list-item>
-      <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-      <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+      <v-list-item prepend-icon="mdi-folder-multiple-outline" title="All Research Projects" value="myfiles" href=""
+        link />
+      <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared" link />
+      <v-list-item prepend-icon="mdi-star" title="Starred" value="starred" link />
     </v-list>
   </v-navigation-drawer>
 </template>
