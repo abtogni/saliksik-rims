0de6c080d0ffc2077257735e8a83344df3d71caa
Kiiyo <85468568+kiiyokami@users.noreply.github.com>
2024-02-15 17:15:10 +0800

    navbar

--- client/src/components/nav_bar.vue
+++ client/src/components/nav_bar.vue
@@ -1,37 +1,29 @@
 <template>
-  <v-navigation-drawer
-    v-if="showDrawer"
-    expand-on-hover
-    rail
-  >
+  <v-navigation-drawer v-if="showDrawer" expand-on-hover rail>
     <!-- Drawer content -->
     <v-list>
-      <v-list-item
-        :title="user.name"
-        :subtitle="user.user_id"
-      ></v-list-item>
+      <v-list-item :title="user.name" :subtitle="user.user_id"></v-list-item>
     </v-list>
 
     <v-divider></v-divider>
 
     <v-list density="compact" nav>
-      <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-      <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-      <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+      <v-list-item prepend-icon="mdi-folder" title="Administrator" href="/administrator" link />
+      <v-list-item prepend-icon="mdi-folder" title="Researcher" href="/researcher" link />
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script setup lang="ts">
 
-  const user = {name: 'Test Account', user_id: '07-11312'}
-  import { ref, watchEffect } from 'vue';
-  import { useRoute } from 'vue-router';
+const user = { name: 'Test Account', user_id: '07-11312' }
+import { ref, watchEffect } from 'vue';
+import { useRoute } from 'vue-router';
 
-  const route = useRoute();
-  const showDrawer = ref(route.path !== '/');
+const route = useRoute();
+const showDrawer = ref(route.path !== '/');
 
-  watchEffect(() => {
-    showDrawer.value = route.path !== '/';
-  });
+watchEffect(() => {
+  showDrawer.value = route.path !== '/';
+});
 </script>
