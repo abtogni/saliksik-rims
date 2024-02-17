9ab8d62004c62490055b6d538662697ddc88a2a1
Kiiyo <85468568+kiiyokami@users.noreply.github.com>
2024-02-17 10:08:58 +0800

    test

--- client/src/components/pages/researcher/status/concept_note_reviewing.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-chip color="primary" append-icon="mdi-information-outline" class="status-txt">Concept Note | Reviewing<v-tooltip activator="parent" location="bottom">
-    The submitted concept note is in review.
-  </v-tooltip></v-chip>
-</template>
