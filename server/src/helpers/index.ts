14785018ba91ec59e3e45f144bf5181141577663
abtogni <andreitogni@gmail.com>
2024-03-11 19:19:07 +0800

    das

--- server/src/helpers/index.ts
+++ server/src/helpers/index.ts
@@ -7,8 +7,12 @@ const SECRET = process.env.JWT_SECRET;
 
 export const random = () => crypto.randomBytes(128).toString("base64");
 export const authentication = (salt: string, password: string) => {
+  const utf8Salt = Buffer.from(salt, 'utf-8').toString('base64');
+  const utf8Password = Buffer.from(password, 'utf-8').toString('base64');
+
   return crypto
-    .createHmac("sha256", [salt, password].join("/"))
-    .update(SECRET)
+    .createHmac("sha256", [utf8Salt, utf8Password].join("/"))
+    .update(Buffer.from(SECRET, 'utf-8'))
     .digest("hex");
 };
+
