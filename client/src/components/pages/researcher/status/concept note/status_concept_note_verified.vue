221863a0fac921374847cc4a8c55620cb1c5e875
abtogni <andreitogni@gmail.com>
2024-03-17 20:57:07 +0800

    .

---
+++ client/src/components/pages/researcher/status/concept note/status_concept_note_verified.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-chip
+    color="primary"
+    prepend-icon="mdi-file-document-outline"
+    class="status-txt"
+    >Concept Note | Verified
+    <div>
+      <v-icon end icon="mdi-check-circle-outline"></v-icon>
+      <v-tooltip activator="parent" location="bottom">
+        The submitted concept note is verified and is due for a presentation.
+      </v-tooltip>
+    </div>
+  </v-chip>
+</template>
